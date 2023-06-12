91026b5737e0e07b9ffa689862d5f1fefa1cec97
WeerawatW <135967943+WeerawatW@users.noreply.github.com>
2023-06-12 17:33:19 +0700

    Delete ai8x-synthesis directory

--- ai8x-synthesis/gen-finger_numbers.sh
+++
@@ -1,7 +0,0 @@
- #!/bin/sh
-DEVICE="MAX78000"
-TARGET="sdk/Examples/$DEVICE/CNN"
-COMMON_ARGS="--device $DEVICE --timer 0 --display-checkpoint --verbose"
-
-#python ai8xize.py --test-dir $TARGET --prefix finger_numbers --checkpoint-file trained/ai85-finger-numbers-qat8-q.pth.tar --config-file networks/hand.yaml --fifo --softmax $COMMON_ARGS "$@"
-python ai8xize.py --test-dir sdk/Examples/MAX78000/CNN --prefix finger_numbers --checkpoint-file trained/ai85-finger-numbers-qat8-q.pth.tar --config-file networks/finger_numbers.yaml --device MAX78000 --compact-data --mexpress --timer 0 --display-checkpoint --verbose --overlap-data --mlator --new-kernel-loader --overwrite --no-unload
--- ai8x-synthesis/quantize_Finger_Numbers.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-python quantize.py trained/ai85-finger-numbers-qat8.pth.tar trained/ai85-finger-numbers-qat8-q.pth.tar --device MAX78000 -v "$@"
